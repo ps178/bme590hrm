dc03dcf6c0c847d04dbf96b9f3fc011ec493324f
Petek Sener <peteksener9@gmail.com>
2018-02-25 12:11:22 -0500

    Module Skeleton

---
+++ ECG_Analysis.py
@@ -0,0 +1,12 @@
+import glob
+
+
+class ECG_Data():
+
+    def __init__(self, File_Name):
+        self.File_Name = File_Name
+
+
+    def Read_File(self):
+        a = input('gi')
+        open(self.File_Name):
