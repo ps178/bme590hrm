a24856ce3f65c60fbe0166429082039b30b9e30e
Petek Sener <peteksener9@gmail.com>
2018-02-26 21:08:02 -0500

    Clean up

--- test_ECG_Analysis.py
+++ test_ECG_Analysis.py
@@ -1,24 +1,27 @@
-#Unit test for ECG data analysis
+# Unit test for ECG data analysis
 
 import pytest
 import numpy as np 
 
 
+# Testing Parse_Class.py file
 def test_Read_File():
-    from Parse_Class import Parse_Files # Get the class
+    from Class_Parse import Parse_Files # Get the class
     trial = Parse_Files(File_Name = 'test_data1.csv') #Using one of the data files as a test
-    Test = np.array(trial.Data_Array)
-    #assert Test[0] == -0.145
-    print(trial.Data_Array)
+    trial.Read_File()
+    assert trial.Data_Array[0,-1] == pytest.approx(-0.145)
+
+def test_Write_File():
+    pass
 
 
+# Testing ECG_Analysis.py file
 def test_Mean_hr_bpm():
     pass
 
 def test_Voltage_Extremes():
     pass
     
-
 def test_Duration():
     pass
 
@@ -28,7 +31,5 @@ def test_Num_Beats():
 def test_Beats():
     pass
 
-def test_Write_File():
-    pass
 
 
